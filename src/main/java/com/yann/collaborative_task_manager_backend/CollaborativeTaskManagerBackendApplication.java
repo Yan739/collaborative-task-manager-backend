0abb62109b0922c9fd9231a6aed9488c3c5eb9b0
Yann Cabrel NGATEU TAGNE <ngateuyann8@gmail.com>
2026-01-29 03:23:20 +0100

    initial commit

---
+++ src/main/java/com/yann/collaborative_task_manager_backend/CollaborativeTaskManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yann.collaborative_task_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeTaskManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeTaskManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yann/collaborative_task_manager_backend/CollaborativeTaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yann.collaborative_task_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeTaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
